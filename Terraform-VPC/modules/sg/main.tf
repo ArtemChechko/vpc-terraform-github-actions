1f009e2114a72ec617288a71b89fb2bc15e42333
ArtemChechko <89904112+ArtemChechko@users.noreply.github.com>
2024-03-21 16:12:57 +0200

    Update sg main.tf

--- Terraform-VPC/modules/sg/main.tf
+++ Terraform-VPC/modules/sg/main.tf
@@ -19,7 +19,7 @@ resource "aws_security_group" "sg" {
     from_port = 22
     to_port = 22
     protocol = "tcp"
-    cidr_blocks = ["176.36.83.49/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   egress  {
     from_port = 0
